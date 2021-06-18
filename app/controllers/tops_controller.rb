88a9b6783950c9233664a9ffd9e8abcadaec91cb
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-18 16:46:27 +0900

    update: topsコントローラ設定（問い合わせ内容のDB保存設定）

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,8 +3,25 @@ class TopsController < ApplicationController
   end
 
   def contact
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(contact_params)
+    if @contact.save
+      redirect_to contact_tops_path, notice: "受付完了"
+    else
+      render :new
+    end 
   end
 
   def tutorial
   end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content, :title)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   root  'tops#index'
 
-  resources :tops, only: [:index] do
+  resources :tops, only: [:index, :create] do
     collection do
       get 'tutorial'
       get 'contact'
