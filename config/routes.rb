c0518b241a661acb8a579b971c8ac4bb899314b9
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-18 16:23:53 +0900

    update: topsコントローラ作成（トップ、問い合わせ、利用ガイドページ、ルーティング設定）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
+  root  'tops#index'
+
+  resources :tops, only: [:index] do
+    collection do
+      get 'tutorial'
+      get 'contact'
+    end
+  end
+
 end
