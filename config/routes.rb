fc7fccf531008e680c3e51e89e7018ec0df8ac74
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-19 19:55:12 +0900

    update: ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root  'tops#index'
 
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   
   resources :tops, only: [:index, :create] do
     collection do
