fb95bda9f96785085cf7caa616e296909e6e87e9
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-19 19:13:43 +0900

    update: DeviseのUserModel作成(カラム最小のため、随時追加予定)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   root  'tops#index'
 
+  devise_for :users
+  
   resources :tops, only: [:index, :create] do
     collection do
       get 'tutorial'
