c69f2753f79dec4636b09ee19fd2010219f69ace
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-19 22:37:48 +0900

    update: userコントローラー作成（index/show）

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  
+  resources :users, only: [:show, :index]
+
   resources :tops, only: [:index, :create] do
     collection do
       get 'tutorial'
