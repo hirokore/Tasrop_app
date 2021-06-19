792c71a90b455f2d2b0f85202ef90191bf13c785
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-20 03:30:39 +0900

    update: ユーザーのフォロー/フォロワー表示ページ及び機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index] do
+    collection do
+      get 'follow/:id', to: 'users#follow', as: 'follow'
+      get 'followed/:id', to: 'users#followed', as: 'followed'
+    end
+  end
   resources :relationships, only: [:create, :destroy]
-
   resources :tops, only: [:index, :create] do
     collection do
       get 'tutorial'
