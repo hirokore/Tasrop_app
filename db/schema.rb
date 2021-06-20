d9b67f54bdc21d61f0ea80fd5f2f52fe579ad490
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-20 13:05:28 +0900

    update: mentorモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_040225) do
+ActiveRecord::Schema.define(version: 2021_06_20_040408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2021_06_20_040225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_customs_on_user_id"
+  end
+
+  create_table "mentors", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_mentors_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -127,6 +134,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_040225) do
   end
 
   add_foreign_key "customs", "users"
+  add_foreign_key "mentors", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
   add_foreign_key "taggings", "tags"
