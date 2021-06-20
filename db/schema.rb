b02651ff8b7ece07296d51d0f3d3ef55e22cbe58
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-20 12:40:15 +0900

    update: customモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_193600) do
+ActiveRecord::Schema.define(version: 2021_06_20_031413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,18 @@ ActiveRecord::Schema.define(version: 2021_06_19_193600) do
     t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
     t.index ["sender_id", "recipient_id"], name: "index_conversations_on_sender_id_and_recipient_id", unique: true
     t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
+
+  create_table "customs", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title", null: false
+    t.boolean "displayed", default: true, null: false
+    t.boolean "use_comment", default: false, null: false
+    t.boolean "use_picture", default: false, null: false
+    t.string "mentor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_customs_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -83,6 +95,7 @@ ActiveRecord::Schema.define(version: 2021_06_19_193600) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "customs", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
 end
